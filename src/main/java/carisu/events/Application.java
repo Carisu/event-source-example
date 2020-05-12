f8461ce5661ee40f74736c934d5b71b68f6ad72c
Carisu <chris.e.j.davis@gmail.com>
2020-05-12 15:19:18 +0100

    Initial commit

---
+++ src/main/java/carisu/events/Application.java
@@ -0,0 +1,11 @@
+package carisu.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application extends SpringApplication {
+    public static void main(String [] args) {
+        new SpringApplication().run(args);
+    }
+}
